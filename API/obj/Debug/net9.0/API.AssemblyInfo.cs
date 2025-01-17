58137c7b540a8742ba5b6468f07cb720070d2b02
Anthon.G <mark.mordido@gmail.com>
2025-01-17 11:48:12 +0800

    First Commit Test

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b9527bca69768040d683461880a4b03d109ba8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3036c3f032b4aa8292ea2b753a1050744a2829ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
